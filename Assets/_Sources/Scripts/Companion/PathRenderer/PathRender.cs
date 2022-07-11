f944fb70577cd66030b4f1cce77164326be186ab
CamillePele <camille.pele.visiteur@lecnam.net>
2022-07-11 13:35:12 +0200

    Part of structure

---
+++ Assets/_Sources/Scripts/Companion/PathRenderer/PathRender.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Companion.PathRenderer {
+	public class PathRender : MonoBehaviour {
+		
+		private Image image;
+		
+		public void Setup(Manager.Grid.Direction from, Manager.Grid.Direction to, PathRender parent) {
+			if (from == to) {
+				return;
+			}
+
+			if (Mathf.Abs((int) from - (int) to) % 2 == 0) { // If go straight
+				image.sprite = Manager.PathRenderer.Instance.straightLine;
+				transform.rotation = parent.transform.rotation;
+			} else { // If turn
+				image.sprite = Manager.PathRenderer.Instance.cornerLine;
+				
+				// Get if turn right or left, handle out of range
+				bool turnLeft = ((int) from - (int) to) % 3 == 0;
+				
+				if (turnLeft) transform.rotation = parent.transform.rotation * Quaternion.Euler(0, 0, -90);
+				// Dont turn right it's the default rotation
+				
+			}
+		}
+	}
+}
---
+++ Assets/_Sources/Scripts/Managers/PathRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Manager {
+	public class PathRenderer : MonoBehaviour {
+
+		#region Singleton
+		
+		public static PathRenderer Instance;
+		
+		void Awake() {
+			if (Instance != null) {
+				Destroy(gameObject);
+			} else {
+				Instance = this;
+			}
+		}
+
+		#endregion
+		
+		[SerializeField] private GameObject _pathPrefab;
+		[SerializeField] public Sprite straightLine;
+		[SerializeField] public Sprite cornerLine;
+
+	}
+}
